cbfb74f049a750d122c881da4c116a20e2cfca75
Rohan3280 <rahul01sept@gmail.com>
2026-04-25 23:56:44 +0530

    Templates

--- playground/views.py
+++ playground/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello World')
+    return render(request, 'hello.html')
     
 
 # Create your views here.
